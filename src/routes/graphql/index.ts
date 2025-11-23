c0ea1ec36bcd1871168fa91c7b8fd96a6cd339c6
andrii <andrii@gmail.com>
2025-11-23 19:08:48 +0300

    feat: add id folder

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { memberTypeLoader } from './member-type/member-type.loader.js';
+import { postLoader } from './post/post.loader.js';
+import { profileLoader } from './profile/profile.loader.js';
+import { createGqlResponseSchema, gqlResponseSchema, MainSchema } from './schemas.js';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import { ILoaders } from './types/context.js';
+import { subscriberLoader, subscriptionLoader } from './user/user.loader.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -14,8 +20,28 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      // return graphql();
+    async handler({ body }) {
+      const query = parse(body.query);
+      const validationResult = validate(MainSchema, query, [depthLimit(5)]);
+
+      if (validationResult?.length) {
+        return { errors: validationResult };
+      }
+
+      const loaders: ILoaders = {
+        memberType: memberTypeLoader(prisma),
+        post: postLoader(prisma),
+        profile: profileLoader(prisma),
+        subscriber: subscriberLoader(prisma),
+        subscription: subscriptionLoader(prisma),
+      };
+
+      return graphql({
+        schema: MainSchema,
+        source: body.query,
+        variableValues: body.variables,
+        contextValue: { prisma, loaders },
+      });
     },
   });
 };
